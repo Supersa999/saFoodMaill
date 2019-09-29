dafeab8d815e121320070598c1ed9f3b50828d6e
masterUNG <master@abc.com>
2019-09-28 20:20:44 -0700

    validator  and Save  finish

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -4,56 +4,94 @@ class Register extends StatefulWidget {
   @override
   _RegisterState createState() => _RegisterState();
 }
-// Explicit
-
-//Method
-Widget registerButton() {
-  return IconButton(
-    icon: Icon(Icons.cloud_upload),
-    onPressed: () {},
-  );
-}
-
-Widget nameText() {
-  return TextFormField(
-    decoration: InputDecoration(
-      icon: Icon(
-        Icons.face,
-        size: 36.0,
-      ),
-      labelText: 'Display Name :',
-      helperText: 'Type Your Name',
-    ),
-  );
-}
-
-Widget userText() {
-  return TextFormField(
-    decoration: InputDecoration(
-      icon: Icon(
-        Icons.account_box,
-        size: 36.0,
-      ),
-      labelText: 'User :',
-      helperText: 'Type Your User',
-    ),
-  );
-}
-
-Widget passwordText() {
-  return TextFormField(
-    decoration: InputDecoration(
-      icon: Icon(
-        Icons.lock,
-        size: 36.0,
-      ),
-      labelText: 'Display password :',
-      helperText: 'Type Your password',
-    ),
-  );
-}
 
 class _RegisterState extends State<Register> {
+  // Explicit
+  final fornKey = GlobalKey<FormState>();
+String name,user,password;
+
+//Method
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {
+
+if (fornKey.currentState.validate()) {
+  fornKey.currentState.save();
+  print('name=$name,user=$user,password=$password');
+
+}
+
+
+      },
+    );
+  }
+
+  Widget nameText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.face,
+          size: 36.0,
+        ),
+        labelText: 'Display Name :',
+        helperText: 'Type Your Name',
+      ),validator: (String value){
+        if (value.isEmpty) {
+          return 'Please Fill Name in the Bank ';
+        } else {
+          return null;
+        }
+      },onSaved: (String value){
+name= value.trim();
+      },
+    );
+  }
+
+  Widget userText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.account_box,
+          size: 36.0,
+        ),
+        labelText: 'User :',
+        helperText: 'Type Your User',
+      ),validator: (String value){
+
+        if (value.isEmpty) {
+          return 'Please Fill User in the Bank';
+        } else {
+          return null;
+        }
+      },onSaved: (String value){
+        user = value.trim();
+      },
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+        ),
+        labelText: 'Display password :',
+        helperText: 'Type Your password',
+      ),validator: (String value){
+if (value.length< 6) {
+  return 'Password More 6 Charecter';
+} else {
+  return null;
+}
+
+      },onSaved: (String value){
+        password = value.trim();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,13 +100,16 @@ class _RegisterState extends State<Register> {
         title: Text('Register'),
         actions: <Widget>[registerButton()],
       ),
-      body: ListView(
-        padding: EdgeInsets.all(50.0),
-        children: <Widget>[
-          nameText(),
-          userText(),
-          passwordText(),
-        ],
+      body: Form(
+        key: fornKey,
+        child: ListView(
+          padding: EdgeInsets.all(50.0),
+          children: <Widget>[
+            nameText(),
+            userText(),
+            passwordText(),
+          ],
+        ),
       ),
     );
   }
